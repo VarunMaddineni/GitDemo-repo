9a2ac5548762f38b8c9dcd0f71e970a6ae8a6aaa
vamaddin <vamaddin@DIN73002976.corp.capgemini.com>
2018-08-06 10:35:26 +0530

    My First commit

---
+++ Day1gitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,14 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Varun");
+greet();
+
+	}
+public static void greet() {
+	System.out.println("Hiii");
+}
+}
