3c46d2d82bc7dc97e89947c07c8ba2ee067345fd
vamaddin <vamaddin@DIN73002976.corp.capgemini.com>
2018-08-06 12:11:46 +0530

    added salesclass

--- Day1gitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day1gitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -3,8 +3,10 @@ package org.cap.demo;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SalesClass class1=new SalesClass();
+		
 System.out.println("Hello Varun");
+class1.show();
 greet();
 
 	}
